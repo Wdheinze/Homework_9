92ffa68146eac9bfb485d67e06b16bdf5f9ad063
Wdheinze <77468808+Wdheinze@users.noreply.github.com>
2021-03-15 10:47:37 -0600

    update3

--- main.js
+++ main.js
@@ -1,45 +1,24 @@
 let rickAndMorty = {
-    "id":216,
-    "url":"https://www.tvmaze.com/shows/216/rick-and-morty",
-    "name":"Rick and Morty",
-    "type":"Animation",
-    "language":"English",
-    "genres":["Comedy","Adventure","Science-Fiction"],
-    "status":"Running",
-    "runtime":30,
-    "premiered":"2013-12-02",
-    "officialSite":"http://www.adultswim.com/videos/rick-and-morty",
-    "schedule":{"time":"23:30","days":["Sunday"]},
-    "rating":{"average":9},
-    "weight":97,
-    "network":{"id":10,"name":"Adult Swim","country":{"name":"United States","code":"US","timezone":"America/New_York"}},
-    "webChannel":null,
-    "externals":{"tvrage":33381,
-    "thetvdb":275274,
-    "imdb":"tt2861424"},
-    "image":{"medium":"https://static.tvmaze.com/uploads/images/medium_portrait/1/3603.jpg",
-    "original":"https://static.tvmaze.com/uploads/images/original_untouched/1/3603.jpg"},
-    "summary":"<p>Rick is a mentally gifted, but sociopathic and alcoholic scientist and a grandfather to Morty; an awkward, impressionable, and somewhat spineless teenage boy. Rick moves into the family home of Morty, where he immediately becomes a bad influence.</p>",
-    "updated":1612746554,"_links":{"self":{"href":"https://api.tvmaze.com/shows/216"},"previousepisode":{"href":"https://api.tvmaze.com/episodes/1839344"}},
-    "_embedded":{"episodes":[{"id":14308,"url":"https://www.tvmaze.com/episodes/14308/rick-and-morty-1x01-pilot",
-    "name":"Pilot",
-    "season":1,
-    "number":1,
-    "type":"regular",
-    "airdate":"2013-12-02",
-    "airtime":"22:30",
-    "airstamp":"2013-12-03T03:30:00+00:00",
-    "runtime":30,"image":
+    "id": 216,
+    "url": "https://www.tvmaze.com/shows/216/rick-and-morty",
+    "name": "Rick and Morty",
+    "type": "Animation",
+    "language": "English"
 }
 
-$(function() {
-    $("button").click(function() {
+$(function () {
+    $("button").click(function () {
         showrickandmorty();
-    }
-});
     });
+});
 
-function showrickandmorty()
-{
-    $("#rickAndMorty").html
+
+function showrickandmorty() {
+    $("#rickandmorty").html("hi")
+    $.getJSON("data/dataset.JSON", function (data) {
+
+        console.log(data.id);
+        $("#rickandmorty").html(data.id);
+
+    });
 }
